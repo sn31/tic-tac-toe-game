e6c553ca5d4a3b1f7cdb704ba4f5039a00a826b7
SkyeN <skye@dames.es>
2018-08-22 16:16:16 -0700

    switch between players

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,9 @@ function Player(mark) {
   this.mark = mark;
 }
 
-function Board() {
-  this.row =[1,2,3];
-  this.col =[1,2,3];
-}
+// function Board() {
+
+// }
 
 function Game() {
   this.playerX = playerX;
@@ -17,32 +16,35 @@ var playerX = new Player("X");
 var playerO = new Player("O");
 
 var currentId = 0;
-var switchPlayer = function(){
+var switchPlayer = function () {
   if (currentId === 1) {
     currentId = 0;
   }
   else {
-  currentId = 1;
+    currentId = 1;
   }
 }
 var players = {
   "X": playerX,
   "O": playerO
 }
-$(document).ready(function(){
-
-  switch(Object.keys(players)[currentId]) {
-    case "X" :
-    $(".col-md-4").click(function(event) {
-      $("#" + event.target.id).text("X");
-    });
-
-    break;
-    case "O" :
-    $(".col-md-4").click(function(event) {
-      $("#" + event.target.id).text("O");
-    });
-
-    break;
+$(document).ready(function () {
+  console.log(currentId);
+  $(this).click(function() {
+    switchPlayer();
+     
+  switch (Object.keys(players)[currentId]) {
+    case "X":
+      $(".col-md-4").click(function (event) {
+        $("#" + event.target.id).text("X");
+      });
+      break;
+    case "O":
+      $(".col-md-4").click(function (event) {
+        $("#" + event.target.id).text("O");
+      });
+      break;
   }
+  })
+
 });
