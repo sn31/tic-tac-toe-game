58a8f0c367449b4b30515cd28ffbec0ee08eceaa
SkyeN <skye@dames.es>
2018-08-22 21:41:20 -0700

    fix double clicking issue

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,14 @@ function Player(mark) {
   this.mark = mark;
 }
 
-// function Board() {
-
-// }
-
-function Game() {
-  this.playerX = playerX;
-  this.playerO = playerO;
-  this.currentPlayer = currentPlayer;
-  this.moves = 1;
+function Board() {
+  this.initialBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+  this.currentBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 }
 var playerX = new Player("X");
 var playerO = new Player("O");
 
+var newBoard = new Board();
 var currentId = 0;
 var switchPlayer = function () {
   if (currentId === 1) {
@@ -28,23 +23,19 @@ var players = {
   "X": playerX,
   "O": playerO
 }
-$(document).ready(function () {
-  console.log(currentId);
-  $(this).click(function() {
-    switchPlayer();
-     
-  switch (Object.keys(players)[currentId]) {
-    case "X":
-      $(".col-md-4").click(function (event) {
-        $("#" + event.target.id).text("X");
-      });
-      break;
-    case "O":
-      $(".col-md-4").click(function (event) {
-        $("#" + event.target.id).text("O");
-      });
-      break;
-  }
-  })
 
+$(document).ready(function () {
+  $(".col-md-4").click(function (event) {
+    switchPlayer();
+    switch (Object.keys(players)[currentId]) {
+      case "X": 
+          $("#" + event.target.id).text("X");
+        break;
+      case "O":
+          $("#" + event.target.id).text("O");
+        break;
+    }
+    console.log(event.target.id);
+  })
+  
 });
